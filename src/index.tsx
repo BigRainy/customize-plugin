64cfae996bf82323194ac3e13f81a70537317b1f
Rain <1203221960@qq.com>
2023-04-23 17:11:16 +0800

    feat: 初始化

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { Button } from "antd";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
+declare global {
+  interface Window {
+    ProjectPlugin: {
+      init: (container: HTMLDivElement) => void;
+      one: (str: string) => string;
+    };
+  }
+}
+
+function ProjectB(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello from Project B!</h1>
+      <Button>提交</Button>
+    </div>
+  );
+}
+
+window.ProjectPlugin = {
+  init: (container) => {
+    const root = ReactDOM.createRoot(container);
+    root.render(<ProjectB />);
+  },
+  one: (str: string) => {
+    return str + "BBB";
+  },
+};
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(<App />);
