35250af37f3d1e76060b2ae7167cbda5986a4682
Rain <1203221960@qq.com>
2023-05-03 20:39:49 +0800

    feat:update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React, { ReactElement } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Button } from "antd-mobile";
+import { Toast, NoticeBar } from "antd-mobile";
 
 declare global {
   interface Window {
@@ -20,25 +20,6 @@ export interface IGood {
   createdAt: Date;
   storeNum: number;
 }
-
-// declare global {
-//   interface Window {
-//     ProjectPlugin: {
-//       init: (container: HTMLDivElement) => void;
-//       one: (str: string) => string;
-//       three: string;
-//     };
-//   }
-// }
-
-// function ProjectB(): JSX.Element {
-//   return (
-//     <div>
-//       <h1>Hello from Project B!</h1>
-//       <Button>提交</Button>
-//     </div>
-//   );
-// }
 
 class CustomizePlugin {
   // 数据定制，包括显示和隐藏
@@ -79,7 +60,7 @@ class CustomizePlugin {
     this.asyncEventStore = [];
   }
 
-  setValueStore = (event: string, value: boolean) => {
+  setValueStore = (event: string, value: any) => {
     this.valueStore.push({
       event,
       value,
@@ -111,81 +92,43 @@ class CustomizePlugin {
   };
 }
 
-// class CustomizePlugin {
-//   eventStore: {
-//     event: string;
-//     value: any;
-//   }[];
-//   constructor() {
-//     this.eventStore = [];
-//   }
-
-//   setValueStore = (event: string, value: boolean) => {
-//     this.eventStore.push({
-//       event,
-//       value,
-//     });
-//   };
-//   setDomStore = (event: string, callback: (value: any) => any) => {
-//     this.eventStore.push({
-//       event,
-//       value: callback,
-//     });
-//   };
-//   setFilterStore = (event: string, callback: (value: any) => any) => {
-//     this.eventStore.push({
-//       event,
-//       value: callback,
-//     });
-//   };
-//   setSyncEventStore = (event: string, callback: (value: any) => any) => {
-//     this.eventStore.push({
-//       event,
-//       value: callback,
-//     });
-//   };
-//   setAsyncEventStore = (event: string, callback: (value: any) => any) => {
-//     this.eventStore.push({
-//       event,
-//       value: callback,
-//     });
-//   };
-// }
-
 const projectPlugin = new CustomizePlugin();
 
+// Plugin 首页样式定制
+projectPlugin.setValueStore("home.page.style", {
+  background: "linear-gradient(to bottom, rgb(255, 200, 0), #eee)",
+});
+
+//  Plugin 轮播图显示或隐藏
 projectPlugin.setValueStore("home.swiper.visible", false);
 
+// Plugin 瀑布流商品展示模块上边区域
 projectPlugin.setDomStore("home.waterfall.top", (ref) => {
-  const element = <Button>提交</Button>;
+  const element = (
+    <NoticeBar
+      content="这条通知可以关闭，这是一条通知，这是一条通知，这是一条通知，这是一条通知"
+      style={{ marginBottom: "10px" }}
+      color="alert"
+      closeable
+    />
+  );
   const anchor = ReactDOM.createRoot(ref);
   anchor.render(element);
 });
 
+// Plugin 首页瀑布流数据过滤
 projectPlugin.setFilterStore("home.waterfall.data", (items: IGood[]) => {
   const newItems = items.filter((item) => !item.title.includes("骆驼"));
   return newItems;
 });
 
+// Plugin 首页分类图片点击事件定制
 projectPlugin.setSyncEventStore("home.sortImage.click", (callback) => {
-  console.log("AAA");
+  Toast.show({
+    icon: "success",
+    content: "跳转成功",
+  });
   callback();
 });
 
 window.ProjectPlugin = projectPlugin;
-
-// window.ProjectPlugin = {
-//   init: (container) => {
-//     const root = ReactDOM.createRoot(container);
-//     root.render(<ProjectB />);
-//   },
-//   one: (str: string) => {
-//     return str + "BBB";
-//   },
-//   three: "UUU",
-// };
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById("root") as HTMLElement
-// );
-// root.render(<App />);
